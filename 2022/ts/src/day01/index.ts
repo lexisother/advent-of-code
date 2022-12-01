51b96c88f777007f48304ecb873e671249f062d7
Alyxia Sother <alyxia@riseup.net>
2022-12-01 07:27:46 +0100

    [2022/ts/d1p1] Add solution

---
+++ 2022/ts/src/day01/index.ts
@@ -0,0 +1,53 @@
+import run from 'aocrunner';
+
+const parseInput = (rawInput: string): string[] => rawInput.split('\n');
+
+const part1 = (rawInput: string): number => {
+  const input = parseInput(rawInput);
+
+  let counts = [];
+  let counter = 0;
+  for (var i = 0; i < input.length; i++) {
+    let line = input[i];
+    if (line == '') {
+      counts.push(counter);
+      counter = 0;
+    } else {
+      counter += parseInt(line);
+    }
+  }
+
+  counts = counts.sort((a, b) => b - a);
+
+  console.log(counts);
+  console.log(counts[0]);
+
+  return counts[0];
+};
+
+const part2 = (rawInput: string): void => {
+  const input = parseInput(rawInput);
+};
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
