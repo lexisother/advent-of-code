4c04363de6df5c9b7560b29f1d15e146dbd715dc
Alyxia <alyxia@riseup.net>
2022-12-04 12:24:11 +0100

    [2022/ts/d4p*] No more duplicate code, fix lint

--- 2022/ts/src/day04/index.ts
+++ 2022/ts/src/day04/index.ts
@@ -10,48 +10,38 @@ const testInput = `
 `;
 const parseInput = (rawInput: string): string => rawInput;
 
-let checker = (arr: number[], target: number[], p2: boolean = false) =>
+let checker = (arr: number[], target: number[], p2 = false): boolean =>
   !p2 ? target.every((v) => arr.includes(v)) : target.some((v) => arr.includes(v));
-let range = (s: number, f: number) => [...Array(f - s + 1)].map((e: number, i: number) => i + s);
+let range = (s: number, f: number): number[] =>
+  [...Array(f - s + 1)].map((_e: number, i: number) => i + s);
 
-const part1 = (rawInput: string): number => {
-  const input = parseInput(rawInput);
+function doPart(input: string, p2 = false): number {
   let pairs = input.split('\n');
 
   let incs = [];
   for (let pair of pairs) {
     let assig = pair.split(',');
-    let id1 = assig[0].split('-').map((i) => parseInt(i));
-    let id2 = assig[1].split('-').map((i) => parseInt(i));
+    let id1 = assig[0].split('-').map((i) => parseInt(i, 10));
+    let id2 = assig[1].split('-').map((i) => parseInt(i, 10));
 
     let sects1 = range(id1[0], id1[1]);
     let sects2 = range(id2[0], id2[1]);
 
-    incs.push(checker(sects1, sects2) || checker(sects2, sects1));
+    incs.push(checker(sects1, sects2, p2) || checker(sects2, sects1, p2));
   }
 
   incs = incs.filter((v) => v === true);
   return incs.length;
+}
+
+const part1 = (rawInput: string): number => {
+  const input = parseInput(rawInput);
+  return doPart(input);
 };
 
 const part2 = (rawInput: string): number => {
   const input = parseInput(rawInput);
-  let pairs = input.split('\n');
-
-  let incs = [];
-  for (let pair of pairs) {
-    let assig = pair.split(',');
-    let id1 = assig[0].split('-').map((i) => parseInt(i));
-    let id2 = assig[1].split('-').map((i) => parseInt(i));
-
-    let sects1 = range(id1[0], id1[1]);
-    let sects2 = range(id2[0], id2[1]);
-
-    incs.push(checker(sects1, sects2, true) || checker(sects2, sects1, true));
-  }
-
-  incs = incs.filter((v) => v === true);
-  return incs.length;
+  return doPart(input, true);
 };
 
 run({
